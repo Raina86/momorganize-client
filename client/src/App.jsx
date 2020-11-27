bf755ec8b26d784d6021e2cc34caed99b36eae07
Raina86 <you@example.com>
2020-11-27 18:09:00 -0500

    pushing done components, need to finish  landing page, register components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import './App.css';
 import ListItems from './ListItems'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+// import Layout from "./components/Layout";
+
+import Footer from './Footer';
+
 // // import NavBar from './nav/NavBar';
 // // import Landing from './landing/Landing';
 
@@ -10,6 +14,19 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 library.add(faTrash)
 
 class App extends React.Component {
+
+//   <div class="jumbotron">
+//   <h1 class="display-4">Hello, world!</h1>
+//   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+//   <hr class="my-4">
+//   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+//   <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+// </div>
+
+
+
+
+
   constructor(props){
     super(props);
     this.state = {
---
+++ client/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Row} from "reactstrap";
+
+const FooterComponent = (props) => {
+  return (
+    <Row className="footer">
+      &copy;{new Date().getFullYear()} MOMANAGEMENT
+    </Row>
+  );
+};
+
+export default FooterComponent;
