97234a14c8d8d7c5c0ce1558fa5a99c754cb69d2
Raina86 <you@example.com>
2020-12-07 01:26:30 -0500

    latest changes

---
+++ client/src/services/profile.js
@@ -0,0 +1,58 @@
+import AxiosClientProvider from './apiClient';
+
+class UserService {
+    constructor() {
+        this.apiClient = new AxiosClientProvider();
+        this.apiClient = this.apiClient.getClient();
+    }
+
+
+
+    getUserSignin(jsonBody, cb) {
+        console.log("response ", jsonBody)
+        this.apiClient.post('user/login', jsonBody).then((response) => {
+            if (response.data) {
+                console.log("response data ", response.data)
+                let dataObj = response.data;
+                if (dataObj.message == "Login successful") {
+                    cb(dataObj);
+                }
+                else {
+                    cb()
+                }
+            } else {
+                cb()
+            }
+        }).catch(err => {
+            console.log('Some error occured in get category', err);
+            cb();
+        })
+    }
+
+    getUserSignUp(jsonBody, cb) {
+        this.apiClient.post('user/register', jsonBody).then((response) => {
+            if (response.data) {
+                let dataObj = response.data;
+                if (dataObj.message == "Success: Account created!") {
+                    console.log("Main response status: ", dataObj);
+                    cb(dataObj);
+                }
+                else {
+                    cb()
+                }
+
+            } else {
+                cb()
+            }
+        }).catch(err => {
+            console.log('Some error occured ', err);
+            cb();
+        })
+    }
+
+    
+
+
+}
+
+export default UserService;
